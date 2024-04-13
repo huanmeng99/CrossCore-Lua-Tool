09dcc7be17f7869785e77ae180c895260511e981
huanmeng99 <100177951+huanmeng99@users.noreply.github.com>
2024-04-13 21:57:55 +0800

    Update Program.cs

--- CrossCore-Lua-Tool/Program.cs
+++ CrossCore-Lua-Tool/Program.cs
@@ -19,6 +19,9 @@ namespace CrossCore
         [Option("nofix", Default = false, HelpText = "Do not fix the file.")]
         public bool NoFix { get; set; }
         
+        [Option("ver-bytes", Default = false, HelpText = "Read version data from file.")]
+        public bool VerBytes { get; set; }
+        
         [Option('h', "help", Default = false, HelpText = "Show help.")]
         public bool Help { get; set; }
     }
@@ -37,6 +40,11 @@ namespace CrossCore
                     else if (o.Decrypt)
                     {
                         LuaScripts.GenDecryptABData(o.InFile, o.OutFile);
+                    }
+                    else if (o.VerBytes)
+                    {
+                        VerMgr verMgr = new VerMgr();
+                        verMgr.LoadVerInfo(o.InFile, o.OutFile);
                     }
                     else if (o.Help)
                     {
